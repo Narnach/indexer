43e65b8cfab4d6a52bddbfbad448c4de8fa30aa2
Wes Oldenbeuving <narnach@gmail.com>
2008-12-10 22:22:22 +0100

    Added record level inverted indexing as Indexer#store

--- lib/indexer.rb
+++ lib/indexer.rb
@@ -1,4 +1,29 @@
-module Indexer
-end
-
 require 'indexer/core_ext/string'
+require 'set'
+
+class Indexer
+  attr_reader :index, :documents
+
+  def initialize
+    @index = Hash.new { |hash, key| hash[key] = Set.new }
+    @documents = {}
+  end
+
+  def store(hash)
+    hash.each do |key, string|
+      tokens = string.tokenize
+      if old_document = documents.delete(key)
+        old_tokens = old_document.tokenize
+        old_tokens.each do |old_token|
+          index[old_token].delete(key)
+          index.delete(old_token) if index[old_token].size==0
+        end
+      end
+      documents[key] = string
+      tokens.each do |token|
+        index[token] << key
+      end
+    end
+    nil
+  end
+end
--- spec/indexer_spec.rb
+++ spec/indexer_spec.rb
@@ -2,4 +2,28 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 require 'indexer'
 
 describe Indexer do
+  describe "#store" do
+    before(:each) do
+      @indexer = Indexer.new
+    end
+
+    it "should index stored sentences" do
+      @indexer.store(:one =>  "First sentence")
+      @indexer.store(:two => "Second sentence")
+      @indexer.index.should == {
+        "sentence" => Set.new([:one, :two]),
+        "first" => Set.new([:one]),
+        "second" => Set.new([:two]),
+      }
+    end
+
+    it "should de-index an overwritten sentence" do
+      @indexer.store(:one => "First sentence")
+      @indexer.store(:one => "Sentence one")
+      @indexer.index.should == {
+        "sentence" => Set.new([:one]),
+        "one" => Set.new([:one]),
+      }
+    end
+  end
 end
